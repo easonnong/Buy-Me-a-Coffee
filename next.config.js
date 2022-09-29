bd1c87bd46e7a96140596dfce18c6566753f8902
easonnong <nongeason@gmail.com>
2022-09-29 22:33:34 +0800

    create header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "s2.coinmarketcap.com",
+      "s3.coinmarketcap.com",
+      "cdn.sanity.io",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Header from "../components/Header";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      {/* <Header /> */}
+      <Header></Header>
+
+      {/* <Trending /> */}
+      <div className="mt-10">Trending</div>
+
+      {/* <CMCtable /> */}
+      <div className="mt-20">CMCtable</div>
+    </div>
+  );
 }
